93d22c1f70a4688be5ee76ed0ba8cdee8ab14090
Masoud <mmtaee64@gmail.com>
2024-10-05 21:56:06 +0330

    build version: v0.0.1

--- backend/settings.py
+++ backend/settings.py
@@ -108,3 +108,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+VERSION = "v0.0.1"
+VERSION_REFERENCE = "5a683a24"
